5758f4d7aaa4a200ecac05e6810dea6b115de7f1
Henri Foko <henrifoko@gmail.com>
2021-09-05 14:17:35 +0100

    correction des erreurs dans la structure du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TableComponent } from './table/table.component';
-import { AuthComponent } from './services/auth/auth.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { MainViewComponent } from './components/main-view/main-view.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent,
-    AuthComponent,
     SideBarComponent,
-    MainViewComponent
+    AuthComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
