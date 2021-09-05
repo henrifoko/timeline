bb8d08077f95463af3d5018f227a95daeba61975
Henri Foko <henrifoko@gmail.com>
2021-09-05 14:06:52 +0100

    initialisation du projet table-timeline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { AuthComponent } from './services/auth/auth.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { MainViewComponent } from './components/main-view/main-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    AuthComponent,
+    SideBarComponent,
+    MainViewComponent
   ],
   imports: [
     BrowserModule
